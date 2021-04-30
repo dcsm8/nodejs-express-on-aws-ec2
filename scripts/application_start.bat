54042992d8c18d13d50c98a5faa922ee8e182d92
David Sánchez <deividsanchez96@hotmail.com>
2021-04-29 22:03:14 -0500

    Update scripts

---
+++ scripts/application_start.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\Administrator\nodejs-express-on-aws-ec2" 
+
+npm install
+
+pm2 start app.js --name app
---
+++ scripts/before_install.bat
@@ -0,0 +1,4 @@
+
+if not exist "C:\Users\Administrator\nodejs-express-on-aws-ec2" mkdir C:\Users\Administrator\nodejs-express-on-aws-ec2
+
+
